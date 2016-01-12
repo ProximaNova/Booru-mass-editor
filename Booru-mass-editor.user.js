ef7184d86f16c1a6163d135d70e94b32f33f00a8
ProximaNova <jaretrudd@gmail.com>
2016-01-12 14:44:13 -0700

    Update Booru-mass-editor.user.js

--- Booru-mass-editor.user.js
+++ Booru-mass-editor.user.js
@@ -1,15 +1,12 @@
 // ==UserScript==
 // @name		Booru Mass Editor
-// @description	Quickly edit image tags on your booru
+// @description	Quickly edit images on older versions of Gelbooru
 // @version	1
 // @author		usernam
-// @include	http://*.booru.org/index.php*
-// @include 	http://gelbooru.com/index.php*
-// @include	http://safebooru.org/index.php*
-// @include	http://rule34.xxx/index.php*
-// @include	http://xbooru.com/index.php*
-// @include	http://pbooru.com/index.php*
-// @include	http://safeponi.com/index.php*
+// @include 	http://rule34.xxx/index.php*
+// @include 	http://*.booru.org/index.php*
+// @include 	http://safebooru.org/index.php*
+// @include 	http://xbooru.com/index.php*
 // @grant 		none 
 // @noframes
 // ==/UserScript==
