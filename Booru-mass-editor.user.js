46afc22c9430f2eceed0556f9694477381bc725f
ProximaNova <jaretrudd@gmail.com>
2016-01-12 14:55:21 -0700

    nope

--- Booru-mass-editor.user.js
+++ Booru-mass-editor.user.js
@@ -4,12 +4,9 @@
 // @version	1
 // @author		usernam
 // @include     http://*.booru.org/index.php*
-// @include 	http://gelbooru.com/index.php*
 // @include     http://safebooru.org/index.php*
 // @include     http://rule34.xxx/index.php*
 // @include     http://xbooru.com/index.php*
-// @include     http://pbooru.com/index.php*
-// @include     http://safeponi.com/index.php*
 // @grant 		none 
 // @noframes
 // ==/UserScript==
