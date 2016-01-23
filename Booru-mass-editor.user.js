d19fb1bd22a8f8c6f9a5049c3f54643f560f8f93
ProximaNova <jaretrudd@gmail.com>
2016-01-23 12:19:45 -0700

    Update Booru-mass-editor.user.js

--- Booru-mass-editor.user.js
+++ Booru-mass-editor.user.js
@@ -32,19 +32,36 @@ var badMyTagsStr = toString(document.getElementById("my-tags").innerHTML)
 var imageExt = document.getElementById("image").src.replace(/^.*\./g, "").toUpperCase();
 
 // Delete text function (could be better):
-function RegDel(a, b) {
+function RegDel(a,b,c,d,e,f,g,h,i) {
     document.body.innerHTML = document.body.innerHTML
     .replace(a, "")
     .replace(b, "")
+    .replace(c, "")
+    .replace(d, "")
+    .replace(e, "")
+    .replace(f, "")
+    .replace(g, "")
+    .replace(h, "")
+    .replace(i, "")
+
 }
 // Deleting text:
-myFunction(/herp/g, /derp/g);
+myFunction(
+/0 comment<a href="#" id="ci" onclick="showHideIgnored\(\d{1,},'ci'\); return false;"> \(0 hidden\)<\/a><br><br><br>/g,
+/Don't like these ads\? Want em removed or want to donate to booru.org\? Check out our Patreon!/g,
+/<a href="index.php\?page=post&amp;s=view&amp;id=\d+"><\/a><br>/g,
+/Source<br>/g,
+/Title<br>/g,
+/Parent<br>/g,
+/ \| /g,
+/<b>Score<\/b>.*Report post.<\/a>/g,
+/<br.*Posted on \d.* by  <a href="index.php\?page=account_profile&amp;uname=.*?">.*?<\/a>/g
+);
 
 // Replacing and hiding:
 document.body.innerHTML =
 document.body.innerHTML
 .replace(/div style="float\: left; margin\: 1em 0"/g, "div style='float: left;'")
-.replace(/<b>Score<\/b>.*Report post.<\/a>/g, "")
 .replace(/<br \/><p id="note-count">/g, "<p id='note-count'>")
 .replace(/<td>\n.*<br>\n.*<input /g, "<td><div style='height:4px;'></div><input ")
 .replace(/>Next Post</g, "><")
@@ -52,29 +69,21 @@ document.body.innerHTML
 .replace(/>Tag History<\/a>/g, ">Tag history</a> &bull; Vote: <a href='#' onclick=\"post_vote('" + ID + "', 'up')\">+</a> <a href='#' onclick=\"post_vote('" + ID+ "', 'down')\">-</a>")
 .replace(/>Note history</g, "><")
 .replace(/>Add note</g, "><")
-.replace(/ \| /g, "")
 .replace(/>Next</g, "><")
 .replace(/>Previous</g, "><")
 .replace(/Previous Post<br>/g, "<br>")
 .replace(/ id="image" onclick="Note.toggle\(\);" style="margin-right\: 70px;"/g, " id='image' onclick='Note.toggle();' style='max-width:800px; margin-right: 70px; position:relative; top:-7px;'")
-.replace(/<br.*Posted on \d.* by  <a href="index.php\?page=account_profile&amp;uname=.*?">.*?<\/a>/g, "")
 .replace(/;}; return false;">Remove<\/a>/g, ";}; return false;\">Remove</a> &bull; ")
 .replace(/>Keep<\/a>/g, ">Favorite</a> &bull; ")
 .replace(/>Edit</g, "><")
 .replace(/<input name="submit" value="Save changes" type="submit">/g, "<input style='position:relative;top:-80px;' name='submit' value='Save changes' type='submit'>")
-.replace(/<a href="index.php\?page=post&amp;s=view&amp;id=\d+"><\/a><br>/g, "")
-.replace(/Source<br>/g, "")
-.replace(/Title<br>/g, "")
-.replace(/Parent<br>/g, "")
 .replace(/type="radio">Safe/g, "type='radio'>Safe (&larr;Rating)")
 .replace(/ type="text">\n		<\/td><\/tr><tr><td>\n		<input name="parent"/g, " type='text'> (&larr;Title)<\/td><\/tr><tr><td><input name='parent'")
 .replace(/ type="text">\n		<\/td><\/tr><tr><td><br>\n		<input name="next_post"/g, " type='text'> (&larr;Parent) (&darr;Source)</td></tr><tr><td><br><input style='display: none;' name='next_post'")
 .replace(/Rating<br>/g, "<br>")
 .replace(/          Id.*<br>/g, "File format: " + imageExt + "<br>")
-.replace(/          Rating.*\n/g, "")
+.replace(/          Rating.*\n/g, "") // Use to display something else
 .replace(/My Tags<br>/g, "<br>")
-.replace(/0 comment<a href="#" id="ci" onclick="showHideIgnored\(\d{1,},'ci'\); return false;"> \(0 hidden\)<\/a><br><br><br>/g, "")
-.replace(/Don't like these ads\? Want em removed or want to donate to booru.org\? Check out our Patreon!/g, "")
 document.getElementById("my-tags").innerHTML = document.getElementById("my-tags").innerHTML.replace(/\+/g, " ");
 document.getElementById("previous_post").style.display = "none";
 document.getElementById("next_post").style.display = "none";
