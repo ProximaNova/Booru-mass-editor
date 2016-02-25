e4b758bd453696949a103ce83a5d2ff00c472800
ProximaNova <jaretrudd@gmail.com>
2016-02-24 20:01:37 -0700

    better regex

--- Booru-mass-editor.user.js
+++ Booru-mass-editor.user.js
@@ -152,11 +152,13 @@ if (width >= 10000 && height >= 10000) {
 if (document.getElementById("title").value.match(/Booru mass uploader/g)) {
     document.getElementById("title").value = "";
 }
-if (document.getElementById("tags").value.match(/\w+((-\w+)+)?\.(jp?g|png|gif)/g)) {
-    document.getElementById("source").value = document.getElementById("tags").value.match(/\w+((-\w+)+)?-?\.(jpe?g|png|gif)/g)
+if (document.getElementById("tags").value.match(/\w+(([^ ]+\w+)+)?\.(jpe?g|png|gif)/g)) {
+    document.getElementById("source").value = document.getElementById("tags").value.match(/\w+(([^ ]+\w+)+)?\.(jpe?g|png|gif)/g)
 }
-
-document.getElementById("tags").value = document.getElementById("tags").value.replace(/ ?\w+((-\w+)+)?-?\.(jpe?g|png|gif) ?/g, " ").replace(/ bad_tag /g, " ") + " ";
+if (document.getElementById("title").value.match(/Probably from 4chan \/s\//g)) {
+    document.getElementById("title").value = "";
+}
+document.getElementById("tags").value = document.getElementById("tags").value.replace(/ ?(\.+)?\w+(([^ ]+\w+)+)?\.(jpe?g|png|gif) ?/g, " ").replace(/ bad_tag /g, " ") + " ";
 document.getElementById("tag_list").innerHTML = document.getElementById("tag_list").innerHTML.replace(/<ul>.*<strong>/g, tagList + "<strong>");
 
 // Hiding:
