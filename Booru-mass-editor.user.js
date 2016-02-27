bcbf5b9fcc4334e5cee2ff29848b7996a3603571
ProximaNova <jaretrudd@gmail.com>
2016-02-27 15:04:00 -0700

    will mass submit edited tags in an endless loop
    It needs to only submit it once

--- Booru-mass-editor.user.js
+++ Booru-mass-editor.user.js
@@ -127,7 +127,7 @@ Replacing:
 .replace(/Previous Post<br>/g, "<br>")
 .replace(/;}; return false;">Remove<\/a>/g, ";}; return false;\">Remove</a> &bull; ")
 .replace(/>Keep<\/a>/g, ">Favorite</a> &bull; ")
-.replace(/<input name="submit" value="Save changes" type="submit">/g, "<input style='position:relative;top:-80px;width:403px;height:100px;font-size:20pt;' name='submit' value='Save changes' type='submit'>")
+.replace(/<input name="submit" value="Save changes" type="submit">/g, "<input  id='SubmitButton' style='position:relative;top:-80px;width:403px;height:100px;font-size:20pt;' name='submit' value='Save changes' type='submit'>")
 .replace(/type="radio">Safe/g, "type='radio'>Safe (&larr;Rating)")
 .replace(/ type="text">\n		<\/td><\/tr><tr><td>\n		<input name="parent"/g, " type='text'> (&larr;Title)<\/td><\/tr><tr><td><input name='parent'")
 .replace(/ type="text">\n		<\/td><\/tr><tr><td><br>\n		<input name="next_post"/g, " type='text'> (&larr;Parent) (&darr;Source)</td></tr><tr><td><br><input style='display: none;' name='next_post'")
