6bf12648059f9d12f515011779fe5d9a709aa0c4
ProximaNova <jaretrudd@gmail.com>
2018-05-10 15:47:41 -0600

    It has a space character in the source code.

--- Code that does stuff.js
+++ Code that does stuff.js
@@ -19,7 +19,7 @@ isb.style.fontSize = "20pt";
 // Delete 0 comments statement
 // = .replace(/0 comment<a href="#" id="ci" onclick="showHideIgnored\(\d{1,},'ci'\); return false;"> \(0 hidden\)<\/a><br><br><br>/g, "")
 let commentsLink = document.getElementById('ci');
-//if (commentsLink.innerHTML == "(0 hidden)" && commentsLink.previousSibling.textContent == "0 comment")
+//if (commentsLink.innerHTML == " (0 hidden)" && commentsLink.previousSibling.textContent == "0 comment")
 //{
   commentsLink.style.display = "none";
   commentsLink.previousSibling.textContent = '';
